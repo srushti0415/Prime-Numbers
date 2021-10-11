39dea0efaaaf2b4ee15c3861b613863ecbb9fa03
srushti0415 <srushtikulkarni15@gmail.com>
2021-10-11 12:03:02 +0530

    updated

---
+++ index.js
@@ -0,0 +1,11 @@
+let b = 2;
+function primenumbers(a) {
+    while (a > b) {
+        if (a / 1 == a && a / a == 1) {
+            console.log("its a prime no")
+        }
+               b++;
+               a/b;
+    }
+}
+primenumbers(5);
